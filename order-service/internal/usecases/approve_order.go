ad05f6f0ba08bc47bf5ec3d19196a182b28e925e
pgnedoy <pavel.gnedoy@uptech.team>
2020-12-06 13:34:26 +0200

    add usecases for order service

---
+++ order-service/internal/usecases/approve_order.go
@@ -0,0 +1,35 @@
+package usecases
+
+import (
+	"context"
+	"errors"
+
+	"github.com/pgnedoy/saga/core/log"
+	"github.com/pgnedoy/saga/order-service/internal/repository"
+)
+
+type ApproveOrder struct {
+	repo repository.Repository
+}
+
+type ApproveOrderConfig struct {
+	Repo repository.Repository
+}
+
+func NewApproveOrder(cfg *ApproveOrderConfig) (*ApproveOrder, error) {
+	if cfg == nil {
+		return nil, errors.New("")
+	}
+
+	if cfg.Repo == nil {
+		return nil, errors.New("")
+	}
+
+	return &ApproveOrder{
+		repo: cfg.Repo,
+	}, nil
+}
+
+func (co *ApproveOrder) Execute(ctx context.Context) {
+	log.Info(ctx, "approve order usecase")
+}
---
+++ order-service/internal/usecases/reject_order.go
@@ -0,0 +1,35 @@
+package usecases
+
+import (
+	"context"
+	"errors"
+
+	"github.com/pgnedoy/saga/core/log"
+	"github.com/pgnedoy/saga/order-service/internal/repository"
+)
+
+type RejectOrder struct {
+	repo repository.Repository
+}
+
+type RejectOrderConfig struct {
+	Repo repository.Repository
+}
+
+func NewRejectOrder(cfg *RejectOrderConfig) (*RejectOrder, error) {
+	if cfg == nil {
+		return nil, errors.New("")
+	}
+
+	if cfg.Repo == nil {
+		return nil, errors.New("")
+	}
+
+	return &RejectOrder{
+		repo: cfg.Repo,
+	}, nil
+}
+
+func (co *RejectOrder) Execute(ctx context.Context) {
+	log.Info(ctx, "reject order usecase")
+}
