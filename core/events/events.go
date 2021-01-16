dac4bcc128c1b7c4a4c0512724b6cef0eb02f518
pgnedoy <pavel.gnedoy@uptech.team>
2021-01-16 15:55:26 +0200

    create base list of events

---
+++ core/events/events.go
@@ -0,0 +1,12 @@
+package events
+
+const (
+	VerifyConsumer = "VERIFY_CONSUMER"
+	ConsumerVerified = "CONSUMER_VERIFIED"
+	CreateTicket = "CREATE_TICKET"
+	TicketCreated = "TICKET_CREATED"
+	AuthorizeCard = "AUTHORIZE_CARD"
+	CardAuthorized = "CARD_AUTHORIZED"
+	ApproveRestaurantOrder = "APPROVE_RESTAURANT_ORDER"
+	ApproveOrder = "APPROVE_ORDER"
+)
